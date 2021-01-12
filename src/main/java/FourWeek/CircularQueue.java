6202022b12a50c8ac56f34a306e8ef8af0e60cfe
dahunkim <kim23222@naver.com>
2021-01-12 21:45:12 +0900

    [추가] int형 CircularQueue 구현

---
+++ src/main/java/FourWeek/CircularQueue.java
@@ -0,0 +1,64 @@
+package FourWeek;
+
+import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
+
+public class CircularQueue {
+
+    private int[] queueArray;
+    private int front;
+    private int rear;
+
+    public CircularQueue() {
+        this(10);
+    }
+
+    public CircularQueue(int size){
+        queueArray = new int[size + 1];
+        front = 0;
+        rear = 0;
+    }
+
+    private int nextIndex(int pos){
+        if (pos == queueArray.length -1){
+            return 0;
+        }else{
+            return pos + 1;
+        }
+    }
+
+    public boolean empty(){
+       return front == rear;
+    }
+
+    public boolean isFull(){
+        return ((rear + 1) % queueArray.length) == front;
+    }
+
+    public void enqueue(int data){
+
+        if (nextIndex(rear) == front){
+            throw new ArrayIndexOutOfBoundsException("큐 꽉참");
+        }
+        rear = nextIndex(rear);
+        queueArray[rear] = data;
+    }
+
+    public int dequeue(){
+
+        if (empty()){
+            throw new ArrayIndexOutOfBoundsException("큐 배열 오류");
+        }
+
+        front = nextIndex(front);
+        return queueArray[front];
+    }
+
+    public int peek() {
+        if (empty()){
+            throw new ArrayIndexOutOfBoundsException("큐 배열이 비어있습니다.");
+        }
+        return queueArray[nextIndex(front)];
+
+    }
+}
